d926c182b1e823ceee279b0cd43afdac5a07a420
Agata872 <2310123801@qq.com>
2025-12-01 09:02:27 +0000

    change

--- client/run_experiment.sh
+++ client/run_experiment.sh
@@ -4,10 +4,20 @@
 ITERATIONS=20       # Number of iterations per gain value
 
 for ((i=1; i<=ITERATIONS; i++))
-    do
-        echo "Running iteration $i"
-        python3 usrp-cal-bf.py
-        echo "Sleeping 5 seconds before next iteration..."
-        sleep 5
+do
+    echo "Running iteration $i"
 
+    # Run both scripts in parallel
+    python3 usrp-cal-bf.py &
+    pid1=$!
+
+    python3 pilot.py &
+    pid2=$!
+
+    # Wait for both to finish
+    wait $pid1
+    wait $pid2
+
+    echo "Sleeping 5 seconds before next iteration..."
+    sleep 5
 done
