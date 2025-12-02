e65653039ad2b052022e7b4cdd79b4fc9c265e7a
Agata872 <2310123801@qq.com>
2025-12-02 12:39:41 +0000

    change

--- client/pilot.py
+++ client/pilot.py
@@ -436,7 +436,7 @@ def get_current_time(usrp):
 
 
 def tx_thread(
-    usrp, tx_streamer, quit_event, phase=[0, 0], amplitude=[0.8, 0.8], start_time=None
+    usrp, tx_streamer, quit_event, phase=None, amplitude=[0.8, 0.8], start_time=None
 ):
     tx_thr = threading.Thread(
         target=tx_ref,
