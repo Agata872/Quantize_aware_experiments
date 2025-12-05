70c043b439d3d9dfb912dea811c16fe9ac993fe5
Agata872 <2310123801@qq.com>
2025-12-05 14:06:41 +0000

    change

--- client/run_experiment.sh
+++ client/run_experiment.sh
@@ -10,9 +10,9 @@ fi
 echo "Running one iteration as role: $ROLE"
 
 if [[ "$ROLE" == "pilot" ]]; then
-    python3 Tx.py
-elif [[ "$ROLE" == "usrp" ]]; then
     python3 Rx.py
+elif [[ "$ROLE" == "usrp" ]]; then
+    python3 Tx.py
 else
     echo "Unknown ROLE '$ROLE'. Expected: pilot or usrp"
     exit 1
