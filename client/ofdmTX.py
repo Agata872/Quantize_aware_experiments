132ae7fb43b6a237c88e90d87d76487b3b15183c
Agata872 <2310123801@qq.com>
2025-12-09 16:34:34 +0000

    Remove obsolete files and update OFDM transmission rate
    - Deleted the image file 'lena_Tx.jpeg' as it is no longer needed.
    - Removed the Python script 'ofdmQam.py' which contained the OFDM modulation and demodulation logic.
    - Deleted the GRC file 'ofdmQamLuan.grc' associated with the OFDM flow graph.
    - Updated the transmission rate in 'ofdmTX.py' from 1e6 S/s to 20e6 S/s to improve transmission speed.

--- client/ofdmTX.py
+++ client/ofdmTX.py
@@ -9,7 +9,7 @@ cp_len = 16
 # USRP Parameters
 frequency = 920e6      # Hz
 gain = 50              # dB
-rate = 1e6             # S/s
+rate = 20e6             # S/s
 duration = 1000          # seconds
 TX_CHANNEL = 1         # 用 0 通道发
 
