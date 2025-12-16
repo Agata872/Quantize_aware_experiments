1a7ec6c2bae76121065e15cc973871e3687b289c
Agata872 <2310123801@qq.com>
2025-12-16 10:57:10 +0100

    change

--- Server/server_process.py
+++ Server/server_process.py
@@ -5,12 +5,10 @@
 # SPDX-License-Identifier: GPL-3.0
 #
 # GNU Radio Python Flow Graph
-# Title: QPSK_RPI_Tx
-# Author: Dramco_Tianzheng
+# Title: Mod_Demod_Server
+# Author: Tianzheng_Miao
 # GNU Radio version: 3.10.10.0
 
-from PyQt5 import Qt
-from gnuradio import qtgui
 from gnuradio import blocks
 import numpy
 from gnuradio import channels
@@ -21,7 +19,6 @@ from gnuradio import gr
 from gnuradio.fft import window
 import sys
 import signal
-from PyQt5 import Qt
 from argparse import ArgumentParser
 from gnuradio.eng_arg import eng_float, intx
 from gnuradio import eng_notation
@@ -31,37 +28,11 @@ from gnuradio.filter import pfb
 
 
 
-class QPSK_RPI_Tx(gr.top_block, Qt.QWidget):
+
+class server_process(gr.top_block):
 
     def __init__(self):
-        gr.top_block.__init__(self, "QPSK_RPI_Tx", catch_exceptions=True)
-        Qt.QWidget.__init__(self)
-        self.setWindowTitle("QPSK_RPI_Tx")
-        qtgui.util.check_set_qss()
-        try:
-            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
-        except BaseException as exc:
-            print(f"Qt GUI: Could not set Icon: {str(exc)}", file=sys.stderr)
-        self.top_scroll_layout = Qt.QVBoxLayout()
-        self.setLayout(self.top_scroll_layout)
-        self.top_scroll = Qt.QScrollArea()
-        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
-        self.top_scroll_layout.addWidget(self.top_scroll)
-        self.top_scroll.setWidgetResizable(True)
-        self.top_widget = Qt.QWidget()
-        self.top_scroll.setWidget(self.top_widget)
-        self.top_layout = Qt.QVBoxLayout(self.top_widget)
-        self.top_grid_layout = Qt.QGridLayout()
-        self.top_layout.addLayout(self.top_grid_layout)
-
-        self.settings = Qt.QSettings("GNU Radio", "QPSK_RPI_Tx")
-
-        try:
-            geometry = self.settings.value("geometry")
-            if geometry:
-                self.restoreGeometry(geometry)
-        except BaseException as exc:
-            print(f"Qt GUI: Could not restore geometry: {str(exc)}", file=sys.stderr)
+        gr.top_block.__init__(self, "Mod_Demod_Server", catch_exceptions=True)
 
         ##################################################
         # Variables
@@ -159,14 +130,6 @@ class QPSK_RPI_Tx(gr.top_block, Qt.QWidget):
         self.connect((self.pfb_arb_resampler_xxx_0, 0), (self.blocks_throttle_0, 0))
         self.connect((self.zeromq_pull_source_0, 0), (self.digital_symbol_sync_xx_0, 0))
 
-
-    def closeEvent(self, event):
-        self.settings = Qt.QSettings("GNU Radio", "QPSK_RPI_Tx")
-        self.settings.setValue("geometry", self.saveGeometry())
-        self.stop()
-        self.wait()
-
-        event.accept()
 
     def get_sps(self):
         return self.sps
@@ -301,30 +264,27 @@ class QPSK_RPI_Tx(gr.top_block, Qt.QWidget):
 
 
 
-def main(top_block_cls=QPSK_RPI_Tx, options=None):
-
-    qapp = Qt.QApplication(sys.argv)
-
+def main(top_block_cls=server_process, options=None):
     tb = top_block_cls()
-
-    tb.start()
-
-    tb.show()
 
     def sig_handler(sig=None, frame=None):
         tb.stop()
         tb.wait()
 
-        Qt.QApplication.quit()
+        sys.exit(0)
 
     signal.signal(signal.SIGINT, sig_handler)
     signal.signal(signal.SIGTERM, sig_handler)
 
-    timer = Qt.QTimer()
-    timer.start(500)
-    timer.timeout.connect(lambda: None)
+    tb.start()
 
-    qapp.exec_()
+    try:
+        input('Press Enter to quit: ')
+    except EOFError:
+        pass
+    tb.stop()
+    tb.wait()
+
 
 if __name__ == '__main__':
     main()
