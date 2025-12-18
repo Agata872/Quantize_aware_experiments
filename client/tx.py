f6dbd2ccf446fe4f659d6f6c90d7957b5cdd006c
Agata872 <2310123801@qq.com>
2025-12-18 13:59:24 +0000

    调整预缓冲时间和最大缓冲时间，以优化数据处理性能

--- client/tx.py
+++ client/tx.py
@@ -28,8 +28,8 @@ TX_BW      = 0
 TX_CHUNK_SAMPS = 4096
 
 # -------- Buffering --------
-PREBUFFER_SEC = 0.2      # 200 ms pre-buffer
-MAXBUFFER_SEC = 0.6        # hard limit (drop oldest)
+PREBUFFER_SEC = 0.1      # 200 ms pre-buffer
+MAXBUFFER_SEC = 0.5        # hard limit (drop oldest)
 
 # =========================================================
 
